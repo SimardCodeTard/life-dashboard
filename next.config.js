34c473248c47356bfa640f8772882d5375483a76
Gerard2par2 <floogles71@gmail.com>
2023-11-08 08:45:30 +0100

    fix(config, weather & calendar): update url for GET requests

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
           source: "/api/v1/:path*",
           headers: [
             {key: "Access-Control-Allow-Credentials", value: "true"},
-            {key: "Access-Control-Allow-Origin", value: "https://localhost:3000"},
+            {key: "Access-Control-Allow-Origin", value: "https://life-dashboard-nine.vercel.app"},
             {key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT"},
             {key: "Access-Control-Allow-Headers", value: "  X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-length, Content-MD5, Content-type, X-API-Version"},
           ]
