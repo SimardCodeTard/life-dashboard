2fccc53d05b28c85fee909980fedcfcc0be865fb
Gerard2par2 <floogles71@gmail.com>
2023-11-08 08:34:22 +0100

    rebase

--- next.config.js
+++ next.config.js
@@ -7,7 +7,11 @@ const nextConfig = {
           source: "/api/v1/:path*",
           headers: [
             {key: "Access-Control-Allow-Credentials", value: "true"},
+<<<<<<< HEAD
             {key: "Access-Control-Allow-Origin", value: "https://life-dashboard-wmcp.vercel.app"},
+=======
+            {key: "Access-Control-Allow-Origin", value: "https://life-dashboard-webapp.vercel.app"},
+>>>>>>> 394ca76 (fix(config): origin)
             {key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT"},
             {key: "Access-Control-Allow-Headers", value: "  X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-length, Content-MD5, Content-type, X-API-Version"},
           ]
