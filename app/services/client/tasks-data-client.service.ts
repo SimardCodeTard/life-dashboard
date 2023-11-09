2533cba3dc69398780fc9ada9396e34e97555c23
Gerard2par2 <floogles71@gmail.com>
2023-11-09 17:47:21 +0100

    refacto(tasks-client): cleanup

--- app/services/client/tasks-data-client.service.ts
+++ app/services/client/tasks-data-client.service.ts
@@ -2,35 +2,35 @@ import axios, { AxiosResponse } from "axios";
 import { Task } from "../../types/task.type";
 import { ObjectId } from "bson";
 
+// Namespace for client-side services to interact with the tasks API.
 export namespace TasksDataClientService {
-
+    // Base URL for the tasks API.
     const apiUrl = 'http://localhost:3000/api/v1/task';
 
+    // Fetches all tasks using a GET request.
     export const fetchAllTasks = (): Promise<Task[]> => {
-        const url = apiUrl;
-        return axios.get(url).then(res => {
-            return res.data;
-        });
+        return axios.get(apiUrl).then(response => response.data as Task[]);
     }
 
+    // Saves a task using a POST request.
     export const saveTask = (task: Task): Promise<AxiosResponse> => {
-        const url = apiUrl + '/new';
-        const options = {
+        const url = `${apiUrl}/new`;
+        return axios.post(url, task, {
             headers: {
                 'Content-Type': 'application/json'
             }
-        };
-        return axios.post(url, task, options);    
+        });    
     }
 
+    // Deletes a task by ID using a DELETE request.
     export const deleteTaskById = (taskId: ObjectId): Promise<AxiosResponse> => {
-        const url = apiUrl + `/delete?taskId=${taskId.toString()}`;
-        return axios.delete(url)
+        const url = `${apiUrl}/delete?taskId=${taskId.toString()}`;
+        return axios.delete(url);
     }
 
+    // Updates a task using a PUT request.
     export const updateTask = (task: Task): Promise<AxiosResponse> => {
-        const url = apiUrl + "/update";
+        const url = `${apiUrl}/update`;
         return axios.put(url, task);
     }
-
 }
