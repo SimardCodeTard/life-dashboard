067919fa362e2ad430ac649b2adf7aadc1769f74
Gerard2par2 <floogles71@gmail.com>
2023-11-09 17:45:27 +0100

    refacto(tasks): cleanup

--- app/services/server/tasks-data.server.service.ts
+++ app/services/server/tasks-data.server.service.ts
@@ -1,87 +1,95 @@
 import { Collection, MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 import { Task } from '../../types/task.type';
 
+// Namespace encapsulating data access operations for tasks.
 export namespace TasksDataServerService {
-
+    // Shared MongoClient instance.
     let client: MongoClient | undefined;
 
+    // Database configuration constants.
     const dbName = 'life-dashboard';
     const collectionName = 'tasks';
 
-    const mongoUrl =  process.env.NODE_ENV === 'development' || process.env.NEXT_PUBLIC_MONGO_DB_URL_LOCAL === undefined
-        ? process.env.NEXT_PUBLIC_MONGO_DB_URL_LOCAL 
-        : process.env.NEXT_PUBLIC_MONGODB_URI;
-    
-    const productionMongoClientOptions = {
+    // MongoDB URL logic to determine if it's running in development or production.
+    const mongoUrl = process.env.NEXT_PUBLIC_MONGO_DB_URL_LOCAL ?? process.env.NEXT_PUBLIC_MONGODB_URI;
+
+    // Options for MongoClient in production environment.
+    const productionMongoClientOptions = process.env.NODE_ENV === 'production' ? {
         serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    } : undefined;
+
+    // Closes the MongoClient and resets the client.
+    export const closeClient = async () => {
+        if (client) {
+            await client.close();
+            client = undefined;
         }
     };
 
-    export const closeClient = async () => {
-        (await getClient()).close();
-        client = undefined;
-    }
-
+    // Retrieves or initializes the MongoClient.
     const getClient = async (): Promise<MongoClient> => {
-        if(!client) {
-            client = await MongoClient.connect(
-                mongoUrl as string, 
-                process.env.NODE_ENV === 'development' 
-                ? undefined
-                : productionMongoClientOptions
+        if (!client) {
+            client = new MongoClient(
+                mongoUrl as string,
+                productionMongoClientOptions
             );
+            await client.connect();
         }
         return client;
-    }
+    };
 
-    const getCollection = async (): Promise<Collection> => await getClient().then((connection: MongoClient) => connection.db(dbName).collection(collectionName))
+    // Gets the collection from the database.
+    const getCollection = async (): Promise<Collection> => {
+        const connection = await getClient();
+        return connection.db(dbName).collection(collectionName);
+    };
 
-    export const findAllTasks = async (): Promise<Task[]> =>{
-        const result = await (await getCollection()).find({}).toArray() as unknown as Task[]
-        closeClient();
+    // Retrieves all tasks from the collection.
+    export const findAllTasks = async (): Promise<Task[]> => {
+        const collection = await getCollection();
+        const tasks = await collection.find({}).toArray() as Task[];
+        await closeClient();
+        return tasks;
+    };
+
+    // Saves a single task to the collection.
+    export const saveTask = async (task: Task): Promise<unknown> => {
+        const { title, completed, deadline } = task;
+        const collection = await getCollection();
+        const result = await collection.insertOne({ title, completed, deadline });
+        await closeClient();
         return result;
     };
 
-    
-    export const saveTask = async (task: Task): Promise<unknown>=>{
-        const result = task && await (await getCollection()).insertOne(
-            {
-                title: task.title,
-                completed: task.completed, 
-                deadline: task?.deadline
-            }
-        );
-        closeClient();
-        return result;
-    }
-
+    // Saves multiple tasks to the collection.
     export const saveTasks = async (tasks: Task[]): Promise<void> => {
-        const insertArray: Task[] = [];
-        for(let task of tasks) {
-            task && insertArray.push({title: task.title, completed: task.completed, deadline: task?.deadline})
+        const insertArray = tasks.map(({ title, completed, deadline }) => ({ title, completed, deadline }));
+        if (insertArray.length > 0) {
+            const collection = await getCollection();
+            await collection.insertMany(insertArray);
         }
-        insertArray.length > 0 && await (await getCollection()).insertMany(insertArray);
-        closeClient();
-    }
+        await closeClient();
+    };
 
+    // Deletes a task by its ID from the collection.
     export const deleteTaskById = async (taskId: string): Promise<void> => {
-        taskId && await (await getCollection()).deleteOne({_id: new ObjectId(taskId)});
-        closeClient();
-    }
+        const collection = await getCollection();
+        await collection.deleteOne({ _id: new ObjectId(taskId) });
+        await closeClient();
+    };
 
+    // Updates a task by its ID in the collection.
     export const updateTask = async (task: Task): Promise<void> => {
-        task && task._id && await (await getCollection()).updateOne(
-            {_id: new ObjectId(task._id)}, 
-            {$set: {
-                title: task.title,
-                deadline: task.deadline,
-                completed: task.completed
-                }
-            }
+        const { _id, title, deadline, completed } = task;
+        const collection = await getCollection();
+        await collection.updateOne(
+            { _id: new ObjectId(_id) },
+            { $set: { title, deadline, completed } }
         );
-        closeClient();
+        await closeClient();
     }
 }
