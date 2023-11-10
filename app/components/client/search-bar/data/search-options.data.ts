07d8020b5603065dbcf0033ccab4741eca785123
Gerard2par2 <floogles71@gmail.com>
2023-11-10 19:13:56 +0100

    refacto(search bar); data

--- app/components/client/search-bar/data/search-options.data.ts
+++ app/components/client/search-bar/data/search-options.data.ts
@@ -1,24 +1,24 @@
 import GoogleIcon from '@mui/icons-material/Google';
 import { SearchOptionType } from '../search-bar.types';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-const SearchOptionData =  {
-    options: {
-        google: {
-            name: 'google',
-            urlCallBack: (userQuery: string): string => {
-                return `https://www.google.com/search?q=${userQuery.replace('/ /g','+')}`;
-            },
-            Icon: GoogleIcon
-        } as SearchOptionType, 
-        youtube: {
-            name: 'youtube',
-            urlCallBack: (userQuery: string): string => {
-                return `https://www.youtube.com/results?search_query=${userQuery.replace('/ /g','+')}`
-            },
-            Icon: YouTubeIcon
-        } 
-    }
-    
-}
+const SearchOptionData: SearchOptionType[] =  [
+    {
+        iconType: 'icon',
+        name: 'google',
+        url: 'https://google.com',
+        queryParamName: 'search',
+        queryWordsSeparator: '+',
+        Icon: GoogleIcon
+    },
+    {
+        iconType: 'icon',
+        name: 'youtube',
+        url: 'ttps://www.youtube.com',
+        queryParamName: '',
+        queryWordsSeparator: '+',
+        path: 'results',
+        Icon: YouTubeIcon
+    } 
+]
 
 export default SearchOptionData;
