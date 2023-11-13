96fb313a8871f8bda6a6a71a580eec45b872c334
Gerard2par2 <floogles71@gmail.com>
2023-11-13 19:00:49 +0100

    fix(calendar): api url

--- app/components/client/calendar/calendar.component.tsx
+++ app/components/client/calendar/calendar.component.tsx
@@ -14,7 +14,7 @@ export default function Calendar() {
     const [selectedDate, setSelectedDate] = useState<string>();
 
     const fetchCalendarEvents = () => {
-        const url = "http://localhost:3000/api/v1/calendar/class";
+        const url =  process.env.NEXT_PUBLIC_API_URL + "calendar/class";
         axios.get(url)
         .then(res => setCalDataMap(CalendarUtils.groupCalEventsByDate(CalendarUtils.mapCalendarEventDTOListToDO(res.data as CalendarEventTypeDTO[]))));
     }
