ed2a21f168da812ae79767493e869947bad0845e
Gerard2par2 <floogles71@gmail.com>
2023-11-18 22:48:26 +0100

    feat(favorites): create components & modules

---
+++ app/components/dashboard-widgets/favorites-bar/favorite-item.tsx
@@ -0,0 +1,7 @@
+import styles from './favorites.module.css'
+
+export default function FavoriteItem() {
+    return <div className={[styles.favoriteItem].join(' ')}>
+
+    </div>
+}
---
+++ app/components/dashboard-widgets/favorites-bar/favorites-bar.tsx
@@ -0,0 +1,7 @@
+import styles from './favorites.module.css'
+
+export default function FavoritesBar() {
+    return <div className={['flex flex-row', styles.favoritesBar].join(' ')}>
+
+    </div>
+}
