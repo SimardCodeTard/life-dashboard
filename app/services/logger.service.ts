b0aa6c46e949dd89ac0d0101ec2b68c18948f5b5
Gerard2par2 <floogles71@gmail.com>
2023-12-06 15:48:04 +0100

    feat(logging): logging level enum

--- app/services/logger.service.ts
+++ app/services/logger.service.ts
@@ -1,10 +1,16 @@
 import { DateTime } from "luxon";
+import { LoggingLevelsEnum } from "../enums/logging-level.enum";
 
 export namespace Logger {
-    const showDebug = (): boolean => true;
-    const showInfo = (): boolean => true;
-    const showWarn = (): boolean => true;
-    const showError = (): boolean => true;
+
+    const loggingLevel = process.env.NEXT_PUBLIC_LOGGING_LEVEL 
+        ? Number(process.env.NEXT_PUBLIC_LOGGING_LEVEL) 
+        : LoggingLevelsEnum.WARN;
+
+    const showDebug = (): boolean => loggingLevel <= LoggingLevelsEnum.DEBUG;
+    const showInfo = (): boolean => loggingLevel <= LoggingLevelsEnum.INFO;
+    const showWarn = (): boolean => loggingLevel <= LoggingLevelsEnum.WARN;
+    const showError = (): boolean => loggingLevel <= LoggingLevelsEnum.ERROR;
 
     export const debug = (message: string) => showDebug() && console.debug(`${DateTime.now().toISO()} [DEBUG] : ${message}`)
     export const info = (message: string) => showInfo() && console.info(`${DateTime.now().toISO()} [INFO] : ${message}`)
