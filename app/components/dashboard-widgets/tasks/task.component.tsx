eb8eb87e81aff799d4e6577b0b941261756cbd04
Gerard2par2 <floogles71@gmail.com>
2023-12-06 17:25:12 +0100

    feat(task): task edit

--- app/components/dashboard-widgets/tasks/task.component.tsx
+++ app/components/dashboard-widgets/tasks/task.component.tsx
@@ -1,11 +1,16 @@
 import { Task } from "@/app/types/task.type";
 import TaskCheckbox from "../../shared/checkbox.component";
 import DeleteIcon from '@mui/icons-material/Delete';
+import { EditNote } from "@mui/icons-material";
 import { TasksDataClientService } from "@/app/services/client/tasks-data-client.service";
 import { DateTime } from "luxon";
-import { Logger } from "@/app/services/logger.service";
+import ModalComponent from "../../shared/modal.component";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function TaskItem ({task, setTasks}: {task: Task, setTasks: (tasks: Task[]) => unknown}) {
+
+    const [editModalOpened, setEditModalOpen] = useState(false);
+    const [taskState, setTaskState] = useState(task);
 
     const deleteButtonClicked = () => {
         task._id && TasksDataClientService.deleteTaskById(task._id)
@@ -15,20 +20,21 @@ export default function TaskItem ({task, setTasks}: {task: Task, setTasks: (task
     }
 
     const updateTaskStatus = (status: boolean) => {
-        task = {...task, completed: status};
+        task = {...taskState, completed: status};
         TasksDataClientService.updateTask(task);
+        setTaskState(task);
     }
 
     const formatTaskDate = (date: string) => DateTime.fromFormat(date, 'yyyy\'-\'MM\'-\'dd');
 
     const deadlineIsPassed = (): boolean => {
-        if(!task.deadline) return false;
-        const taskDate = formatTaskDate(task.deadline);
+        if(!taskState.deadline) return false;
+        const taskDate = formatTaskDate(taskState.deadline);
         const today = DateTime.now();
         return today.toMillis() > taskDate.toMillis();
     }
 
-    const deadlineDate = task.deadline ? formatTaskDate(task.deadline)  : undefined;
+    const deadlineDate = taskState.deadline ? formatTaskDate(taskState.deadline)  : undefined;
 
     let [day, month, year]: string[] | undefined[] = deadlineDate
         ? [deadlineDate.day.toString(), deadlineDate.month.toString(), deadlineDate.year.toString()] 
@@ -40,14 +46,35 @@ export default function TaskItem ({task, setTasks}: {task: Task, setTasks: (task
         month = '0' + month
     }
 
+    const onTaskEditFormSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        const newTaskName = (event.target as any)[0].value;
+        task = { ...taskState, title: newTaskName };
+        setTaskState(task);
+        TasksDataClientService.updateTask(task);
+        setEditModalOpen(false);
+    }
+
+    const taskTitleInputChange = (e: ChangeEvent<HTMLInputElement>) => setTaskState({...taskState, title: e.currentTarget.value});
+
     return(
         <div className="flex flex-col task-item p-3">
             <div className=" flex space-x-2 items-center">
-                <TaskCheckbox updateTaskStatus={updateTaskStatus} completed={task.completed}></TaskCheckbox> 
-                <p>{task.title}</p>
-                <DeleteIcon onClick={deleteButtonClicked} className="cursor-pointer text-[rgba(255,255,255,0.2)] text-base hover:text-[rgba(255,255,255,0.6)]"></DeleteIcon>
+                <TaskCheckbox updateTaskStatus={updateTaskStatus} completed={taskState.completed}></TaskCheckbox> 
+                <p>{taskState.title}</p>
+                <span>
+                    <EditNote onClick={() => setEditModalOpen(true)} className="cursor-pointer text-[rgba(255,255,255,0.2)] text-base hover:text-[rgba(255,255,255,0.6)]"></EditNote>
+                    <DeleteIcon onClick={deleteButtonClicked} className="cursor-pointer text-[rgba(255,255,255,0.2)] text-base hover:text-[rgba(255,255,255,0.6)]"></DeleteIcon>
+                </span>
             </div>
-            <p className={'text-sm' + ` ${deadlineIsPassed() ? 'text-red-500/75' : 'text-[rgb(var(--text-lighter-rgb))]'}`} >{task.deadline && `Deadline: ${day}/${month}/${year}`}</p>
+            <p className={'text-sm' + ` ${deadlineIsPassed() ? 'text-red-500/75' : 'text-[rgb(var(--text-lighter-rgb))]'}`} >{taskState.deadline && `Deadline: ${day}/${month}/${year}`}</p>
+            
+            <ModalComponent modalOpened={editModalOpened} setModalOpened={setEditModalOpen}>
+                <form onSubmit={onTaskEditFormSubmit}>
+                    <input autoFocus={true} value={taskState.title} onChange={taskTitleInputChange} className='h-6 w-5/6 mb-2 bg-[rgba(255,255,255,0.2)] rounded p-1' type="text" placeholder='Name'></input>
+                    <button className='h-6 w-5/6 mt-2 bg-[rgba(255,255,255,0.3)] rounded'>Save</button>
+                </form>
+            </ModalComponent>
         </div>
     )
 }
