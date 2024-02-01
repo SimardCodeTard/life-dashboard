7e5fed49a0ccdd2e12d177089b23200168244838
Gerard2par2 <floogles71@gmail.com>
2024-02-01 16:58:38 +0100

    fix(*): auth

--- app/services/client/openai-client.service.ts
+++ app/services/client/openai-client.service.ts
@@ -2,6 +2,7 @@ import assert from "assert";
 import { ChatCompletionUserMessageParam } from "openai/resources/index.mjs";
 import { Logger } from "../logger.service";
 import { axiosClientService } from "./axios.client.service";
+import { ChatMessage } from "@/app/types/chat.type";
 
 export namespace clientOpenAIService {
     try{
@@ -11,7 +12,7 @@ export namespace clientOpenAIService {
     }
     const url = process.env.NEXT_PUBLIC_API_URL + "/chat" as string;
 
-    export const startNewConversation = () => axiosClientService.GET(url + '/new').then(res => res.data);
+    export const startNewConversation = () => axiosClientService.GET<ChatMessage[]>(url + '/new').then(res => res.data);
 
-    export const sendMessage = (message: ChatCompletionUserMessageParam) => axiosClientService.POST(url, {role: 'user', content: message}).then(res => res.data);
+    export const sendMessage = (message: ChatCompletionUserMessageParam): Promise<ChatMessage[]> => axiosClientService.POST<ChatMessage[]>(url, {role: 'user', content: message}).then(res => res.data);
 }
