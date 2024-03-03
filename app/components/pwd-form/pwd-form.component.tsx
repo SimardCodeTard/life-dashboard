1a0ebc7df0eb9b4d4731e6f1ac770bf65b7a448e
Simard <83214983+Gerard2par2@users.noreply.github.com>
2024-03-03 15:24:43 +0100

    feat/evo-auth (#40)
    - save and use pwd in local storage
    - disable button during login, display error when invalid pwd

--- app/components/pwd-form/pwd-form.component.tsx
+++ app/components/pwd-form/pwd-form.component.tsx
@@ -1,38 +1,53 @@
 'use client'
 
 import { clientLoginService } from "@/app/services/client/login.client.service";
-import { FormEvent } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import Cookies from 'js-cookie';
 import { Logger } from "@/app/services/logger.service";
 
 export default function PWDForm() {
 
+    const [isLoggingIn, setIsLoggingIn] = useState(false);
+    const [passwordInvalid, setPasswordInvalid] = useState(false); 
+
 
     const onSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        const password = (e.target as any)[0].value; 
-        (e.target as any)[0].value = '';
-        console.log(password)
-        clientLoginService.login(password)
-        .then((res: {token: string} | false) => {
-            if (res !== false) {
+        const savedPasswordEncrypted = localStorage.getItem('pwd');
+        const savedPassword = savedPasswordEncrypted ? new TextDecoder().decode(new Uint8Array(Buffer.from(savedPasswordEncrypted, 'hex'))) : '';
+        if(savedPassword === '') {
+            e.preventDefault();
+            const password = (e.target as any)[0].value; 
+            (e.target as any)[0].value = '';
+            setIsLoggingIn(true);
+            clientLoginService.login(password)
+            .then((res) => {
                 Logger.debug('Login successful');
                 // Save the token in a cookie
                 Cookies.set('token', res.token, { expires: 1 });
+
+                // Save the password in the local storage
+                localStorage.setItem('pwd', password);
+
+                setIsLoggingIn(false);
+
                 // Redirect to the dashboard
                 window.location.href = '/dashboard';
-            } else {
+            }).catch(() => {
+                setIsLoggingIn(false);
+                setPasswordInvalid(true);
                 Logger.debug('Login failed');
-                throw new Error('')
-            }
-        })
-        .catch(() => console.log('failed to login'));
+            })
+        }
     }
 
     return (
-        <form onSubmit={onSubmit} className="flex flex-col space-y-4 bg-[var(--card-background)] p-3 rounded-md shadow-md">
-            <input autoFocus className="rounded-sm bg-[rgba(255,255,255,0.2)] backdrop-blur-xl border-white/20-1" type="password" />
-            <button type="submit" className="bg-[rgba(255,255,255,0.2)] rounded-sm hover:bg-[rgba(255,255,255,0.4)]">Login</button>
-        </form>
+        <>
+            <form onSubmit={onSubmit} className="flex flex-col space-y-4 bg-[var(--card-background)] p-3 rounded-md shadow-md">
+                <input autoFocus className="rounded-sm bg-[rgba(255,255,255,0.2)] backdrop-blur-xl border-white/20-1" type="password" />
+                <button type="submit" className="bg-[rgba(255,255,255,0.2)] rounded-sm hover:bg-[rgba(255,255,255,0.4)]" 
+                disabled={isLoggingIn as boolean}>Login</button>
+            </form>
+            {passwordInvalid && <p className="text-red-500">Invalid password</p>}
+        </>
     )
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+
+import { useState } from "react";
 import PWDForm from "../components/pwd-form/pwd-form.component";
 import Card from "../components/shared/card.component";
+import { clientLoginService } from "../services/client/login.client.service";
 
 export default function LoginPage() {
     return <div className="flex flex-col pt-24 items-center h-screen">
@@ -7,7 +11,7 @@ export default function LoginPage() {
             <div className="p-4 space-y-4 flex flex-col w-full items-center">
                 <h1>Welcome to Life Dashboard</h1>
                 <p>Please enter your password to continue</p>
-                <PWDForm />    
+                <PWDForm/>    
             </div>
         </Card>
     </div>
