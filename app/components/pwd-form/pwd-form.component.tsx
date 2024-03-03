96456da712f3c8d292ac87249099c72838135937
Gerard2par2 <floogles71@gmail.com>
2024-03-03 12:46:01 +0100

    fix(auth): move auto login to login/page, display loading animation during auto login

--- app/components/pwd-form/pwd-form.component.tsx
+++ app/components/pwd-form/pwd-form.component.tsx
@@ -40,10 +40,6 @@ export default function PWDForm() {
         }
     }
 
-    useEffect(() => {
-        clientLoginService.automaticallyAuthenticate();
-    })
-
     return (
         <>
             <form onSubmit={onSubmit} className="flex flex-col space-y-4 bg-[var(--card-background)] p-3 rounded-md shadow-md">
