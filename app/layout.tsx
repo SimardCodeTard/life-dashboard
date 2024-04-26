29f332a7117b540631fb678a6be4e00d99f3bf93
Gerard2par2 <floogles71@gmail.com>
2024-04-26 19:18:10 +0200

    feat(*): navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NavBar from './components/dashboard-widgets/navbar.component'
+import NavBar from './components/dashboard-widgets/navbar/navbar.component'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar></NavBar>
         {children}
       </body>
     </html>
