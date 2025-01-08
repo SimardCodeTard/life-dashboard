8133d83bb0e53219936b04c7796b4299fd7b68c3
Simard <floogles71@gmail.com>
2025-01-08 20:48:48 +0100

    call patchCalendarSourceURL when saving new calendar source

--- app/api/v1/calendar/source/new/route.ts
+++ app/api/v1/calendar/source/new/route.ts
@@ -2,4 +2,4 @@ import { serverCalendarDataService } from "@/app/services/server/calendar-data.s
 import { CalendarSourceType } from "@/app/types/calendar.type";
 import { handleAPIError } from "@/app/utils/api.utils";
 
-export const POST = (req: Request) => (req.json() as Promise<CalendarSourceType>).then(serverCalendarDataService.saveNewCalendarSource).then(Response.json).catch(handleAPIError);
+export const POST = (req: Request) => (req.json() as Promise<CalendarSourceType>).then(source => ({...source, url: serverCalendarDataService.patchCalendarSourceURL(source.url)})).then(serverCalendarDataService.saveNewCalendarSource).then(Response.json).catch(handleAPIError);
