72f50ef8f496ba4617568fd8e8be57c1ce19c481
Simard <83214983+SimardCodeTard@users.noreply.github.com>
2025-01-12 17:18:05 +0100

    fix(calendar): force route /calendar/source to dynamic (#63)

--- app/api/v1/calendar/source/route.ts
+++ app/api/v1/calendar/source/route.ts
@@ -1,4 +1,6 @@
 import { serverCalendarDataService } from "@/app/services/server/calendar-data.server.service";
 import { handleAPIError } from "@/app/utils/api.utils";
 
+export const dynamic = 'force-dynamic'
+
 export const GET = () => serverCalendarDataService.findAllCalendarSources().then(Response.json).catch(handleAPIError);
