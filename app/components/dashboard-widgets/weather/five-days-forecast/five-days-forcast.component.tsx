a590c795351bdd9828ad9363ba04b1f6f0bdd5f1
Simard <floogles71@gmail.com>
2025-03-14 21:14:21 +0100

    fix(weather): missing key prop for element in iterator

--- app/components/dashboard-widgets/weather/five-days-forecast/five-days-forcast.component.tsx
+++ app/components/dashboard-widgets/weather/five-days-forecast/five-days-forcast.component.tsx
@@ -5,8 +5,8 @@ import './five-days-forecast.scss'
 
 export default function FiveDaysForecast({weatherForecastsData}: {weatherForecastsData: ForecastWeatherApiResponse[]}) {
     return <div className="five-days-forecast">
-        {weatherForecastsData.map(dayForecast => {
-                return <DayForecast dayForecastData={dayForecast}></DayForecast>
+        {weatherForecastsData.map((dayForecast, key) => {
+                return <DayForecast key={key} dayForecastData={dayForecast}></DayForecast>
         })
     }</div>
 }
